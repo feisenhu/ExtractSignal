f93afe8ddbe242c905cc615c058020d740ec6416
feisenhu <53603353+feisenhu@users.noreply.github.com>
2020-03-13 18:12:43 +0100

    Try Full new Eta fit

--- ExtractSignal.C
+++ ExtractSignal.C
@@ -586,7 +586,7 @@ void DrawProjection(std::vector<TH2D> VecHistos, std::vector<Double_t> vec_proj_
               // // fEtaFit->SetParLimits(5,0.0,0.02);    // (*)
               fEtaFit->SetParLimits(4,0.544,0.55);
               // fEtaFit->SetParLimits(5,0.0,0.01);
-              fEtaFit->SetParLimits(6,0,0.01);
+              fEtaFit->SetParLimits(6,0,1);
               projXeta->Fit("fEtaFit","QRMNE0");
               projXeta->Fit("fEtaFit","QRMNE0");
               projXeta->Fit("fEtaFit","QRMNE0");
